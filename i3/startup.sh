9e8072b9f30099fc521753a87a2ad07e8ec0654b
Chris Hairfield <chairfield@gmail.com>
2015-09-12 12:15:25 -0400

    found out how to title terminator windows, solves previous problems

--- i3/startup.sh
+++ i3/startup.sh
@@ -1,15 +1,17 @@
 #!/bin/zsh
 
-# Load my layouts
+# Workspace 1: Open a vim window with x-today.txt and x-learnings.txt in vsplit
 i3-msg workspace 1: today/learnings
 i3-msg append_layout ~/.i3/workspace-1.json
+terminator --command "vim -O ~/x-today.txt ~/x-learnings.txt" -T "Today Notes/Learnings" &
+
+# Workspace 2: Open a vim window with x-goals.txt on the left and my calendar spreadsheet on the right
 i3-msg workspace 2: goals/calendar
 i3-msg append_layout ~/.i3/workspace-2.json
+terminator --command "vim ~/x-goals.txt" -T "Goals" &
+gnumeric x-calendar.gnumeric &
+
+# Workspace 10: Open chromium and point it to my Anki decks
 i3-msg workspace 10: Anki
 i3-msg append_layout ~/.i3/workspace-10.json
-
-# Start to programs to be swallowed
-terminator --command "vim -O ~/x-today.txt ~/x-learnings.txt" &
-gnumeric x-calendar.gnumeric &
 chromium --new-window --force-device-scale-factor=2 https://ankiweb.net/decks/ &
-terminator --command "vim ~/x-goals.txt" &
