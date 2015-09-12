4e91eb3732b2db0a61a54461af36d45d071af890
Chris Hairfield <chairfield@gmail.com>
2015-09-12 11:52:22 -0400

    including Anki in my startup spree

--- i3/startup.sh
+++ i3/startup.sh
@@ -5,8 +5,11 @@ i3-msg workspace 1: today/learnings
 i3-msg append_layout ~/.i3/workspace-1.json
 i3-msg workspace 2: goals/calendar
 i3-msg append_layout ~/.i3/workspace-2.json
+i3-msg workspace 10: Anki
+i3-msg append_layout ~/.i3/workspace-10.json
 
 # Start to programs to be swallowed
 terminator --command "vim -O ~/x-today.txt ~/x-learnings.txt" &
 gnumeric x-calendar.gnumeric &
+chromium --new-window --force-device-scale-factor=2 https://ankiweb.net/decks/ &
 terminator --command "vim ~/x-goals.txt" &
