edd67432db90f5db287ab58829383b915192ea8c
Chris Hairfield <chairfield@gmail.com>
2015-09-12 19:07:27 -0400

    swapped learnings and goals

--- i3/startup.sh
+++ i3/startup.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
 # Workspace 1: Open a vim window with x-today.txt and x-learnings.txt in vsplit
-i3-msg workspace 1: today/learnings
+i3-msg workspace 1: today/goals
 i3-msg append_layout ~/.i3/workspace-1.json
-terminator --command "vim -O ~/x-today.txt ~/x-learnings.txt" -T "Today Notes/Learnings" &
+terminator --command "vim -O ~/x-today.txt ~/x-goals.txt" -T "To Do" &
 
 # Workspace 2: Open a vim window with x-goals.txt on the left and my calendar spreadsheet on the right
-i3-msg workspace 2: goals/calendar
+i3-msg workspace 2: learnings/calendar
 i3-msg append_layout ~/.i3/workspace-2.json
-terminator --command "vim ~/x-goals.txt" -T "Goals" &
+terminator --command "vim ~/x-learnings.txt" -T "Learnings" &
 gnumeric x-calendar.gnumeric &
 
 # Workspace 10: Open chromium and point it to my Anki decks
