f3deda5ab47b168fd7b3b6c0a28c656b5e4764fa
Chris Hairfield <chairfield@gmail.com>
2015-11-01 14:54:13 -0700

    culling science project for the moment

--- i3/startup.sh
+++ i3/startup.sh
@@ -17,9 +17,9 @@ i3-msg append_layout ~/.i3/workspace-4.json
 chromium --new-window &
 
 # Workspace 5: Project
-i3-msg workspace 5: project
-i3-msg append_layout ~/.i3/workspace-5.json
-terminator --command "vim ~/science/mood-measure.txt" -T "Science" &
+#i3-msg workspace 5: project
+#i3-msg append_layout ~/.i3/workspace-5.json
+#terminator --command "vim ~/science/mood-measure.txt" -T "Science" &
 
 # Workspace 10: Open chromium and point it to my Anki decks
 #i3-msg workspace 10: Anki
