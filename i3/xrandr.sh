6e5f81efd95deed220e534e1245efff81ec67210
Chris Hairfield <chairfield@gmail.com>
2015-12-14 20:48:39 -0700

    disabling laptop screen

--- i3/xrandr.sh
+++ i3/xrandr.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 if [ 3 -eq $(xrandr | grep '\Wconnected' | awk '{ print $1 }' | wc -l) ]; then
-  xrandr --output eDP-1 --pos 0x1500 --primary --output DP-2-1 --auto --rotate left --pos 1920x0 --output DP-2-2 --auto --pos 0x420
+    xrandr --output eDP-1 --off --output DP-2-1 --auto --rotate left --pos 1920x0 --output DP-2-2 --auto --pos 0x420
 else
-    xrandr --output eDP-1 --pos 0x0 --primary --output DP-2-1 --off --output DP-2-2 --off
+    xrandr --output eDP-1 --auto --pos 0x0 --primary --output DP-2-1 --off --output DP-2-2 --off
 fi
