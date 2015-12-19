b47a65df2b6f32ad232541a884108e1d6edf4079
Chris Hairfield <chairfield@gmail.com>
2015-12-18 20:02:37 -0700

    crappy attempt at fixing the random xrandr bug by breaking it up into multiple commands -- still need to test against projector

--- i3/xrandr.sh
+++ i3/xrandr.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
 if [ 3 -eq $(xrandr | grep '\Wconnected' | awk '{ print $1 }' | wc -l) ]; then
-    xrandr --output eDP-1 --off --output DP-2-1 --auto --rotate left --pos 1920x0 --output DP-2-2 --auto --pos 0x420
+    #xrandr --output eDP-1 --off --output DP-2-1 --auto --rotate left --pos 1920x0 --output DP-2-2 --auto --pos 0x420
+    xrandr --output DP-2-1 --auto --output DP-2-2 --auto
+    xrandr --output eDP-1 --off
+    xrandr --output DP-2-1 --rotate left --pos 1920x0 --output DP-2-2 --pos 0x420
 else
     xrandr --output eDP-1 --auto --pos 0x0 --primary --output DP-2-1 --off --output DP-2-2 --off
 fi
