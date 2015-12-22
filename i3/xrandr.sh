8461d6d79ad76a311b72099a1005336157cf6a3f
Chris Hairfield <chairfield@gmail.com>
2015-12-21 17:40:10 -0700

    including my laptop screen to run htop

--- i3/xrandr.sh
+++ i3/xrandr.sh
@@ -3,8 +3,8 @@
 if [ 3 -eq $(xrandr | grep '\Wconnected' | awk '{ print $1 }' | wc -l) ]; then
     #xrandr --output eDP-1 --off --output DP-2-1 --auto --rotate left --pos 1920x0 --output DP-2-2 --auto --pos 0x420
     xrandr --output DP-2-1 --auto --output DP-2-2 --auto
-    xrandr --output eDP-1 --off
-    xrandr --output DP-2-1 --rotate left --pos 1920x0 --output DP-2-2 --pos 0x420
+    xrandr --output eDP-1 --auto --pos 0x420
+    xrandr --output DP-2-1 --rotate left --pos 3840x0 --output DP-2-2 --pos 1920x420 --primary
 else
     xrandr --output eDP-1 --auto --pos 0x0 --primary --output DP-2-1 --off --output DP-2-2 --off
 fi
