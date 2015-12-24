092c874727b2fbe77cd6a1f744baff874a2b31e4
Chris Hairfield <chairfield@gmail.com>
2015-12-24 13:06:50 -0700

    no longer using i3 layouts on startup

--- i3/startup.sh
+++ i3/startup.sh
@@ -1,30 +1,10 @@
 #!/bin/zsh
 
-# Workspace 1: Open a vim window with x-today.txt and x-learnings.txt in vsplit
-i3-msg workspace 1: today/goals
-i3-msg append_layout ~/.i3/workspace-1.json
-terminator --command "vim -O ~/h/today.txt ~/h/goals.txt" -T "To Do" &
-
-# Workspace 2: Open a vim window with x-goals.txt on the left and my calendar spreadsheet on the right
-i3-msg workspace 2: learnings/calendar
-i3-msg append_layout ~/.i3/workspace-2.json
-terminator --command "vim ~/h/learnings.txt" -T "Learnings" &
-gnumeric ~/h/calendar.gnumeric &
-
-# Workspace 4: Chromium
-#i3-msg workspace 4
-#i3-msg append_layout ~/.i3/workspace-4.json
-#chromium --new-window &
-
-# Workspace 5: Project
-#i3-msg workspace 5: project
-#i3-msg append_layout ~/.i3/workspace-5.json
-#terminator --command "vim ~/science/mood-measure.txt" -T "Science" &
-
-# Workspace 10: Open chromium and point it to my Anki decks
-#i3-msg workspace 10: Anki
-#i3-msg append_layout ~/.i3/workspace-10.json
-#anki &
-
-# End on Workspace 3
-i3-msg workspace 3
+#####################################################################################
+# Navigate to a workspace:
+#i3-msg workspace 1
+#i3-msg workspace 1: today/goals
+#
+# Append a layout created by i3-save-tree:
+#i3-msg append_layout ~/path/to/layout.file
+#####################################################################################
