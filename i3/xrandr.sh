13858c0a1d0ff2feac3b87743ec65878dc9409c5
Chris Hairfield <chairfield@gmail.com>
2016-01-09 13:24:10 -0700

    saving new monitor config

--- i3/xrandr.sh
+++ i3/xrandr.sh
@@ -2,9 +2,12 @@
 
 if [ 3 -eq $(xrandr | grep '\Wconnected' | awk '{ print $1 }' | wc -l) ]; then
     #xrandr --output eDP-1 --off --output DP-2-1 --auto --rotate left --pos 1920x0 --output DP-2-2 --auto --pos 0x420
+
+    # Turn on the extra screens
     xrandr --output DP-2-1 --auto --output DP-2-2 --auto
-    xrandr --output eDP-1 --auto --pos 0x420
-    xrandr --output DP-2-1 --rotate left --pos 3840x0 --output DP-2-2 --pos 1920x420 --primary
+
+    xrandr --output eDP-1 --auto --pos 0x840
+    xrandr --output DP-2-1 --rotate right --pos 3000x0 --output DP-2-2 --rotate left --pos 1920x0 --primary
 else
     xrandr --output eDP-1 --auto --pos 0x0 --primary --output DP-2-1 --off --output DP-2-2 --off
 fi
