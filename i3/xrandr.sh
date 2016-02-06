cb544bfb5acee2a0105ba62926051fa04148fe64
Chris Hairfield <chairfield@gmail.com>
2016-02-05 17:19:41 -0700

    fixing positioning bug for eDP-1

--- i3/xrandr.sh
+++ i3/xrandr.sh
@@ -10,8 +10,9 @@ xrandr | grep -v '^[ ]' | grep '^[a-zA-Z]*\-' | awk '{ print $1 }' | xargs bash 
 
 if [ 3 -eq $CONNECTED_DISPLAY_COUNT ]; then
     # TODO: Verify that the three are eDP-1, DP-2-1, and DP-2-2
-    xrandr --output eDP-1 --auto --pos 0x840 --primary # Turn on the main screen as primary
+    xrandr --output eDP-1 --auto --primary # Turn on the main screen as primary
     xrandr --output DP-2-1 --auto --output DP-2-2 --auto # Turn on the extra screens
+    xrandr --output eDP-1 --pos 0x840 # I think turning it on as the only monitor automatically sets -pos to 0x0, so I do it after turning on the other guys
     xrandr --output DP-2-1 --rotate right --pos 3000x0 --output DP-2-2 --rotate left --pos 1920x0
 elif [ 2 -eq $CONNECTED_DISPLAY_COUNT ]; then
     xrandr --output eDP-1 --auto --pos 0x0 --primary
