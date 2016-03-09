7634612ee869dbded8aa78004093cb46324ae686
Chris Hairfield <chairfield@gmail.com>
2016-03-09 14:37:36 -0700

    xrandr updating location to avoid catching mouse at top of screens

--- i3/xrandr.sh
+++ i3/xrandr.sh
@@ -12,14 +12,14 @@ if [ 4 -eq $CONNECTED_DISPLAY_COUNT ]; then
     # TODO: Verify that the four are eDP-1, DP-2-1, DP-2-2, and DP-1
     xrandr --output eDP-1 --off
     xrandr --output DP-1 --auto --output DP-2-1 --auto --output DP-2-2-8 --auto --primary # Turn on the extra screens
-    xrandr --output DP-2-1 --rotate right --pos 2520x640 \
+    xrandr --output DP-2-1 --rotate right --pos 2520x320 \
            --output DP-2-2-8 --rotate right --pos 1080x0 \
-           --output DP-1 --rotate left --pos 0x640
+           --output DP-1 --rotate left --pos 0x320
 elif [ 3 -eq $CONNECTED_DISPLAY_COUNT ]; then
     # TODO: Verify that the three are eDP-1, DP-2-1, and DP-2-2-8
     xrandr --output eDP-1 --off
     xrandr --output DP-2-1 --auto --output DP-2-2-8 --auto --primary # Turn on the extra screens
-    xrandr --output DP-2-1 --rotate right --pos 1440x640 --output DP-2-2-8 --rotate right --pos 0x0
+    xrandr --output DP-2-1 --rotate right --pos 1440x320 --output DP-2-2-8 --rotate right --pos 0x0
 elif [ 2 -eq $CONNECTED_DISPLAY_COUNT ]; then
     xrandr --output eDP-1 --auto --pos 0x0 --primary
     xrandr --output $(echo $CONNECTED_DISPLAYS | tail -1) --auto --same-as eDP-1
