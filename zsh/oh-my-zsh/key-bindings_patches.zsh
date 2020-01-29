6028e419fe23e9f26f8b212e359f59de86de0b60
Chris Hairfield <chairfield@gmail.com>
2020-01-28 21:02:51 -0800

    taking over my zsh config, using Antigen and Oh My Zsh

--- zsh/oh-my-zsh/key-bindings_patches.zsh
+++
@@ -1,6 +0,0 @@
-# These make up/down-arrow autocomplete take into account what I have already typed
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
-
-# Think execute 'docker-compose kill' and auto-populate with 'docker-compose rm -f'
-bindkey '^X^N' accept-and-infer-next-history
